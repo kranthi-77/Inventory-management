0718cf4466656e4ace45cbadced447f330ca4ccd
kranthi-77 <vkranthi485@gmail.com>
2025-06-23 15:18:49 +0530

    fix

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "https://s3-invman.s3.ap-south-1.amazonaws.com",
+        hostname: "s3-invman.s3.ap-south-1.amazonaws.com",
         port: "",
         pathname: "/**",
       },
